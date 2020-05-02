57f3dd99b4ea71eede7577b9e0de67c3c79b8e69
Abdulrahman Y. idlbi <adlogi@gmail.com>
2020-05-02 05:50:20 +0300

    Add levels

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Container, Row, Col, Button, DropdownButton, Dropdown, ButtonGroup } from 'react-bootstrap';
 import logo from '../logo.svg';
 
-export default function HomePage({onStartClick}) {
+export default function HomePage({level, onChangeLevel, onStartClick}) {
   return (
-    <Container fluid>
+    <Container>
       <Row>
         <Col className="mt-5 text-center">
           <img src={logo} className="App-logo" alt="logo" />
@@ -20,6 +20,19 @@ export default function HomePage({onStartClick}) {
           <Button variant="secondary w-100" onClick={onStartClick}>GET STARTED</Button>
         </Col>
       </Row>
+      <Row>
+        <Col xs={10} className="offset-1 my-5 text-center">
+          <DropdownButton title={capitalizeFirst(level)} variant="secondary w-100">
+            <Dropdown.Item onClick={() => onChangeLevel('easy')} className="w-100">Easy</Dropdown.Item>
+            <Dropdown.Item onClick={() => onChangeLevel('medium')} className="w-100">Medium</Dropdown.Item>
+            <Dropdown.Item onClick={() => onChangeLevel('hard')} className="w-100">Hard</Dropdown.Item>
+          </DropdownButton>
+        </Col>
+      </Row>
     </Container>
   )
+}
+
+function capitalizeFirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
