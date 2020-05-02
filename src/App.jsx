ddab203ab1b5832415e6d5b1556e7b1e09c52f9a
Abdulrahman Y. idlbi <adlogi@gmail.com>
2020-05-02 18:35:26 +0300

    Clean up App states

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import HomeScreen from './components/HomeScreen';
+import Loading from './components/Loading';
 import Question from './components/Question';
 import Result from './components/Result';
 import './App.css';
@@ -7,20 +8,32 @@ import './App.css';
 const QUESTIONS_COUNT = 10;
 
 function App() {
-  
-  // stage -1: Intro screen
-  // stage 0 ... <QUESTIONS_COUNT> - 1: Question screen
-  const [stage, setStage] = useState(-1);
-  // result 'p': still playing
-  // result 's': success
-  // result 'w': wrong answer
-  // result 't': time's up!
-  const [result, setResult] = useState('p');
+  // questionIndex: 0 ... <QUESTIONS_COUNT> - 1
+  const [questionIndex, setQuestionIndex] = useState(0);
+  // screen 'i': intro
+  // screen 'f': fetching data
+  // screen 'p': still playing
+  // screen 's': success
+  // screen 'w': wrong answer
+  // screen 't': time's up!
+  const [screen, setScreen] = useState('i');
   // quiz questions/answers as retrieved from triviaDB
   const [quiz, setQuiz] = useState(null);
   const [points, setPoints] = useState(0);
   const [level, setLevel] = useState('easy');
   const [category, setCategory] = useState('9');
+
+  const restart = () => {
+    setScreen('i');
+    setQuiz(null);
+    setQuestionIndex(0);
+    setPoints(0);
+  }
+
+  const handleStartClick = () => {
+    setScreen('f');
+    fetchQuiz(level, category);
+  }
 
   const fetchQuiz = (difficulty = 'easy', category = '9') => {
     // example: https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple&encode=url3986
@@ -30,47 +43,48 @@ function App() {
       .then(res => res.json())
       .then(data => {
         setQuiz(data.results);
-        setStage(0);
+        setScreen('p');
+        setQuestionIndex(0);
       });
   }
 
-  const restart = () => {
-    setStage(-1);
-    setResult('p');
-    setQuiz(null);
-    setPoints(0);
+  switch (screen) {
+    // fetching quiz
+    case 'f':
+      return (<Loading />);
+    // playing, showing questions
+    case 'p':
+      return (
+        <Question
+          stage={questionIndex}
+          total={QUESTIONS_COUNT}
+          question={quiz[questionIndex]}
+          points={points}
+          setPoints={setPoints}
+          showNextQuestion={() => setQuestionIndex(questionIndex + 1)}
+          endGame={setScreen}
+        />
+      );
+    
+    case 's':   // game ends: success
+    case 'w':   // game ends: wrong answer
+    case 't':   // game ends: time's up
+      return (
+        <Result outcome={screen} points={points} onRestart={restart}></Result>
+      );
+    // game intro
+    case 'i':
+    default:
+      return (
+        <HomeScreen
+          category={category}
+          onChangeCategory={setCategory}
+          level={level}
+          onChangeLevel={setLevel}
+          onStartClick={handleStartClick}
+        />
+      );
   }
-
-  const handleStartClick = () => {
-    fetchQuiz(level, category);
-  }
-
-  if (stage === -1)
-    return (
-      <HomeScreen
-        category={category}
-        onChangeCategory={setCategory}
-        level={level}
-        onChangeLevel={setLevel}
-        onStartClick={handleStartClick}
-      />
-    );
-
-  if (result === 'p' && stage < QUESTIONS_COUNT) {
-    return (
-      <Question
-        stage={stage}
-        total={QUESTIONS_COUNT}
-        question={quiz[stage]}
-        points={points}
-        setPoints={setPoints}
-        showNextQuestion={() => setStage(stage + 1)}
-        endGame={setResult}
-      />
-    );
-  }
-  
-  return <Result outcome={result} points={points} onRestart={restart}></Result>
 }
 
 export default App;
