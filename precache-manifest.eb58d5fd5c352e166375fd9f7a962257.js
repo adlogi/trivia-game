50efd35d45ef3920206265e055bf546e97450f05
Abdulrahman Y. idlbi <adlogi@gmail.com>
2020-05-21 12:58:36 +0300

    Updates

---
+++ precache-manifest.eb58d5fd5c352e166375fd9f7a962257.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a29717bbc586d1ac13e2189be515282",
+    "url": "/trivia-app/index.html"
+  },
+  {
+    "revision": "b7cec5605164b8845fc4",
+    "url": "/trivia-app/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "b19013344ee919f906c2",
+    "url": "/trivia-app/static/css/main.6811623d.chunk.css"
+  },
+  {
+    "revision": "b7cec5605164b8845fc4",
+    "url": "/trivia-app/static/js/2.9cdca840.chunk.js"
+  },
+  {
+    "revision": "6485e28f1bc40c1e743bb4de5f286310",
+    "url": "/trivia-app/static/js/2.9cdca840.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b19013344ee919f906c2",
+    "url": "/trivia-app/static/js/main.81a43816.chunk.js"
+  },
+  {
+    "revision": "a9e9ded1e32ef07c5d45",
+    "url": "/trivia-app/static/js/runtime-main.8863014f.js"
+  }
+]);
